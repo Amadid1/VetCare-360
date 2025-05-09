c38e69f3a43a6e0e4f9330b5e5ff628da7f9db6e
Amadid9 <soulaymanamadid40@gmail.com>
2025-05-09 01:15:33 +0100

    Ajout des fichiers frontend

--- backend/models/Visit.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const visitSchema = new mongoose.Schema({
-  petId: { type: mongoose.Schema.Types.ObjectId, ref: 'Pet', required: true },
-  date: { type: Date, required: true },
-  description: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Visit', visitSchema);
